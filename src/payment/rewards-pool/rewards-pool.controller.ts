45b7ae13715b3ada366ab9e28ee1e8b91ffa4dd7
Patches429 <patches@adion.ai>
2025-04-30 17:34:27 +0800

    reward pool types

--- src/payment/rewards-pool/rewards-pool.controller.ts
+++ src/payment/rewards-pool/rewards-pool.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('rewards-pool')
-export class RewardsPoolController {}
+import { Controller, Get, Post, Body, Param } from "@nestjs/common"
+import { AppendTokenDto, CreateRewardsPoolDto, UpdateRewardsPoolDto } from "./rewards-pool.dto"
+@Controller("/api/v1/rewards-pool")
+export class RewardsPoolController {
+    @Post("/create")
+    async createPool(@Body() body: CreateRewardsPoolDto) {
+        // TODO: Implement the logic to create a new rewards pool
+        // return this.rewardsPoolService.createPool(body)
+    }
+    @Post("/update")
+    async updatePool(@Body() body: UpdateRewardsPoolDto) {
+        // TODO: Implement the logic to update a rewards pool
+        // return this.rewardsPoolService.updatePool(body)
+    }
+    @Post("/append-token")
+    async appendToken(@Body() body: AppendTokenDto) {
+        // TODO: Implement the logic to append a token to a rewards pool
+        // return this.rewardsPoolService.appendToken(body)
+    }
+    @Get("/")
+    async getPools() {
+        // TODO: Implement the logic to get all rewards pools
+        // return this.rewardsPoolService.getPools()
+    }
+    @Get("/:mint_address")
+    async getPoolByMintAddress(@Param("mint_address") mint_address: string) {
+        // TODO: Implement the logic to get a rewards pool by mint address
+        // return this.rewardsPoolService.getPoolByMintAddress(mint_address)
+    }
+}
--- src/payment/rewards-pool/rewards-pool.dto.ts
+++ src/payment/rewards-pool/rewards-pool.dto.ts
@@ -1,14 +1,50 @@
-export class RewardAllocateAccount {
+import { ApiProperty, PickType } from "@nestjs/swagger"
+
+export enum RewardAllocateRoles {
+    PLATFORM = "platform",
+    BUYBACK = "buyback",
+    INVITER = "inviter",
+    DEVELOPER = "developer",
+    CUSTOMIZED = "customized",
+}
+
+export class RewardAllocateRatio {
+    @ApiProperty({ description: "Address of the account" })
     address: string
+    @ApiProperty({ description: "Ratio of tokens to be allocated to the pool" })
     ratio: number
-    type: "developer" | "platform" | "customized"
+    @ApiProperty({ description: "Type of the account", enum: RewardAllocateRoles })
+    type: RewardAllocateRoles
+}
+
+export class CreateRewardsPoolDto {
+    @ApiProperty({ description: "Token mint address" })
+    token: string
+    @ApiProperty({ description: "Amount of tokens to be allocated to the pool" })
+    amount: number
+    @ApiProperty({
+        description:
+            "Unit price of the token, in usdc, currently we only support integer and precision decimal is 9, for example, if you want set 0.1 usdc per token, you should pass this param as 0.1 * 10^9 = 100000000",
+    })
+    unit_price: number
+    @ApiProperty({ description: "Ratio of tokens to be allocated to the pool" })
+    ratio_detail: RewardAllocateRatio[]
+}
+
+export class UpdateRewardsPoolDto extends PickType(CreateRewardsPoolDto, ["unit_price", "ratio_detail", "token"]) {}
+
+export class AppendTokenDto {
+    @ApiProperty({ description: "IP ID" })
+    ip_id: number
+    @ApiProperty({ description: "Amount of tokens to be appended to the pool" })
+    append_amount: number
 }
 
 export class RewardModelDto {
     id: number
-    ratio_detail: RewardAllocateAccount[]
     token_address: string
     pool_address: string
+    ratio_detail: RewardAllocateRatio[]
     created_at: Date
     updated_at: Date
 }
