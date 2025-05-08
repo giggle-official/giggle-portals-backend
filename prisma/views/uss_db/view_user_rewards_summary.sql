46a64ad52d16d5647880a03861cbc103b8aebf38
Patches429 <patches@adion.ai>
2025-05-08 18:46:43 +0800

    refactor: optimize user rewards summary view by restructuring SQL queries and removing unnecessary ticker field

--- prisma/views/uss_db/view_user_rewards_summary.sql
+++ prisma/views/uss_db/view_user_rewards_summary.sql
@@ -2,41 +2,61 @@ WITH `t1` AS (
   SELECT
     `a`.`user` AS `user`,
     `a`.`token` AS `token`,
-    `a`.`ticker` AS `ticker`,
     sum(`a`.`rewards`) AS `rewards`,
     sum(`a`.`locked_rewards`) AS `locked`,
-    sum(`a`.`released_rewards`) AS `released`,
-    sum(IFNULL(`b`.`withdrawn`, 0)) AS `withdrawn`
+    sum(`a`.`released_rewards`) AS `released`
   FROM
-    (
-      `uss_db`.`user_rewards` `a`
-      LEFT JOIN `uss_db`.`user_rewards_withdraw` `b` ON(
-        (
-          (`a`.`token` = `b`.`token`)
-          AND (`a`.`user` = `b`.`user`)
-          AND (`b`.`status` IN ('pending', 'completed'))
-        )
-      )
-    )
+    `uss_db`.`user_rewards` `a`
   WHERE
     (`a`.`ticker` <> 'usdc')
   GROUP BY
     `a`.`user`,
-    `a`.`token`,
-    `a`.`ticker`
+    `a`.`token`
+),
+`t2` AS (
+  SELECT
+    `b`.`user` AS `user`,
+    `b`.`token` AS `token`,
+    sum(IFNULL(`b`.`withdrawn`, 0)) AS `withdrawn`
+  FROM
+    `uss_db`.`user_rewards_withdraw` `b`
+  WHERE
+    (`b`.`status` IN ('pending', 'completed'))
+  GROUP BY
+    `b`.`user`,
+    `b`.`token`
+),
+`t3` AS (
+  SELECT
+    `a`.`user` AS `user`,
+    `a`.`token` AS `token`,
+    `a`.`rewards` AS `rewards`,
+    `a`.`locked` AS `locked`,
+    `a`.`released` AS `released`,
+    IFNULL(`b`.`withdrawn`, 0) AS `withdrawn`
+  FROM
+    (
+      `t1` `a`
+      LEFT JOIN `t2` `b` ON(
+        (
+          (`b`.`user` = `a`.`user`)
+          AND (`b`.`token` = `a`.`token`)
+        )
+      )
+    )
 )
 SELECT
-  md5(((`a`.`user` + `a`.`token`) + `a`.`ticker`)) AS `id`,
+  md5(((`a`.`user` + `a`.`token`) + `b`.`ticker`)) AS `id`,
   `a`.`user` AS `user`,
   `a`.`token` AS `token`,
-  `a`.`ticker` AS `ticker`,
   `a`.`rewards` AS `rewards`,
   `a`.`locked` AS `locked`,
   `a`.`released` AS `released`,
   `a`.`withdrawn` AS `withdrawn`,
+  `b`.`ticker` AS `ticker`,
   `b`.`token_info` AS `token_info`
 FROM
   (
-    `t1` `a`
+    `t3` `a`
     LEFT JOIN `uss_db`.`ip_library` `b` ON((`b`.`token_mint` = `a`.`token`))
   )
