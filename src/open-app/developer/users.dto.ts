584f40512857e17cc654b8cc242ab2857972f9db
Patches429 <patches@adion.ai>
2025-07-28 11:49:25 +0800

    feat: make user_id and email optional in GetUserTokenDto to support mutually exclusive field validation

--- src/open-app/developer/users.dto.ts
+++ src/open-app/developer/users.dto.ts
@@ -1,12 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger"
-import { IsEmail, IsString } from "class-validator"
+import { IsEmail, IsOptional, IsString } from "class-validator"
 
 export class GetUserTokenDto {
     @ApiProperty({ description: "user id, user_id and email are mutually exclusive", required: false })
+    @IsOptional()
     @IsString()
     user_id?: string
 
     @ApiProperty({ description: "email, user_id and email are mutually exclusive", required: false })
+    @IsOptional()
     @IsEmail()
     email?: string
 
