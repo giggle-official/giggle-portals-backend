1aae7caccd4c5f752534c854bc07a9f33ddebf09
Patches429 <patches@adion.ai>
2025-09-16 15:33:50 +0800

    feat: add WebSocket support for launch plan parsing and enhance API for market maker delegation management

--- src/ip-library/market-maker/market-maker.dto.ts
+++ src/ip-library/market-maker/market-maker.dto.ts
@@ -1,6 +1,6 @@
-import { ApiProperty } from "@nestjs/swagger"
+import { ApiProperty, OmitType } from "@nestjs/swagger"
 import { ip_token_delegation_status } from "@prisma/client"
-import { IsArray, IsEmail, IsNotEmpty, IsNumber, IsString } from "class-validator"
+import { IsArray, IsEmail, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator"
 import { PaginationDto } from "src/common/common.dto"
 import { LaunchIpTokenDto } from "../ip-library.dto"
 
@@ -73,14 +73,50 @@ export class CancelIpDelegationDto {
     delegation_id: number
 }
 
+export class MarketMakerDto extends OmitType(ListMarketMakerResponseByAdminDto, ["email"]) {}
+
 export class IpDelegationDto {
+    @ApiProperty({
+        description: "id of the delegation",
+    })
     id: number
+    @ApiProperty({
+        description: "name of the ip",
+    })
     ip_name: string
+    @ApiProperty({
+        description: "ticker of the ip",
+    })
     ip_ticker: string
+
+    @ApiProperty({
+        description: "id of the ip",
+    })
     ip_id: number
+
+    @ApiProperty({
+        description: "owner of the ip",
+    })
     owner: string
+
+    @ApiProperty({
+        description: "status of the delegation",
+        enum: ip_token_delegation_status,
+    })
     status: ip_token_delegation_status
+
+    @ApiProperty({
+        description: "Market maker info",
+        type: MarketMakerDto,
+    })
+    market_maker_info: MarketMakerDto
+    @ApiProperty({
+        description: "Created at",
+    })
     created_at: Date
+    @ApiProperty({
+        description: "Updated at",
+    })
     updated_at: Date
 }
 
@@ -101,3 +137,29 @@ export class LaunchIpTokenByMarketMakerDto extends LaunchIpTokenDto {
 }
 
 export class IpDelegationQueryDto extends PaginationDto {}
+
+export class AllocateDelegationToMarketMakerDto {
+    @IsNotEmpty()
+    @ApiProperty({ description: "id of the delegation" })
+    delegation_id: number
+
+    @IsNotEmpty()
+    @ApiProperty({ description: "id of the market maker" })
+    market_maker_id: number
+}
+
+export class AllIpDelegationsQueryDto extends PaginationDto {
+    @IsOptional()
+    @ApiProperty({
+        description: "filter by status of the delegation",
+        required: false,
+        enum: ip_token_delegation_status,
+    })
+    @IsEnum(ip_token_delegation_status)
+    status?: ip_token_delegation_status
+
+    @IsOptional()
+    @ApiProperty({ description: "filter by market maker of the delegation", required: false })
+    @IsString()
+    market_maker?: string
+}
