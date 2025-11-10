d045b3fb521d18be0415a87d8eff7789fd64499b
Patches429 <patches@adion.ai>
2025-11-10 15:16:59 +0800

    feat: add top-up URL and payment callback endpoints to Credit2c API, including request parameters and response handling

--- src/payment/credit-2c/credit-2c.controller.ts
+++ src/payment/credit-2c/credit-2c.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Req, UseGuards } from "@nestjs/common"
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Query, Req, UseGuards } from "@nestjs/common"
 import { Credit2cService } from "./credit-2c.service"
 import { UserJwtExtractDto } from "src/user/user.controller"
 import { ApiTags } from "@nestjs/swagger"
 import { AuthGuard } from "@nestjs/passport"
 import { Request } from "express"
+import { IsWidgetGuard } from "src/auth/is_widget.guard"
+import { Credit2cPaymentCallbackDto } from "./credit-2c.dto"
 
 @Controller("/api/v1/credit-2c")
 @ApiTags("Credit2c Management")
@@ -18,7 +20,14 @@ export class Credit2cController {
 
     @Get("/top-up-url")
     @UseGuards(AuthGuard("jwt"))
-    async topUpUrl(@Req() req: Request) {
-        return this.credit2cService.getTopUpUrl(req.user as UserJwtExtractDto)
+    async topUpUrl(@Req() req: Request, @Query("amount") amount: number, @Query("order_id") order_id: string) {
+        return this.credit2cService.getTopUpUrl(req.user as UserJwtExtractDto, amount, order_id)
+    }
+
+    @Post("/payment-callback")
+    @UseGuards(IsWidgetGuard)
+    @HttpCode(HttpStatus.OK)
+    async paymentCallback(@Body() body: Credit2cPaymentCallbackDto) {
+        return this.credit2cService.processPaymentCallback(body)
     }
 }
